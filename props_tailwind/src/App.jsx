ed19115d11c5c0ff27eea7d7d84f94f01658fb29
Yash <kudveyash@gmail.com>
2024-06-12 13:03:43 +0530

    handling props

--- props_tailwind/src/App.jsx
+++ props_tailwind/src/App.jsx
@@ -2,11 +2,27 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Card from "./components/Card";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  let myObj = {
+    userName: "Yash",
+    age: 21,
+  };
+
+  let newArr = [1, 2, 3];
+
+  return (
+    <>
+      <h1 className="bg-orange-400 text-black p-6 rounded-2xl">
+        Intro to tailwind
+      </h1>
+      <Card userName="Yash" />
+      {/* <Card /> */}
+    </>
+  );
 }
 
 export default App;
