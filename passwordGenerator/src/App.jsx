8f2c1f51902dc3371e322f7e85ed1941f0c3df4a
Yash <kudveyash@gmail.com>
2024-06-12 19:24:31 +0530

    minor updates

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -9,12 +9,25 @@ function App() {
   const passwordGenerator = useCallback(() => {
     let pass = "";
     let str = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
+
+    if (numAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*()+=/*-[]{}/?";
+
+    for (let i = 1; i <= array.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+
+      pass = str.charAt(char);
+    }
+
+    setPassword(pass);
   }, [length, numAllowed, charAllowed, setPassword]);
   return (
     <>
       <h1 className="text-4xl text-center text-white font-extrabold">
         Password Generator
       </h1>
+
+      <div className="w-full max-w-md"></div>
     </>
   );
 }
