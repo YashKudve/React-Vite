74ad513d7896343afa41dff1c620b55558b96a3c
Yash <kudveyash@gmail.com>
2024-06-12 18:04:13 +0530

    use of hook useState and useCallback

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,5 +1,22 @@
+import { useState, useCallback } from "react";
+
 function App() {
-  return <></>;
+  const [length, setLength] = useState(8);
+  const [numAllowed, setNumAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
+  }, [length, numAllowed, charAllowed, setPassword]);
+  return (
+    <>
+      <h1 className="text-4xl text-center text-white font-extrabold">
+        Password Generator
+      </h1>
+    </>
+  );
 }
 
 export default App;
