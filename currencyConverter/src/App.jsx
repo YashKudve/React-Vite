d07962a3691553c72c0b31401226566cb6ccbbae
Yash <kudveyash@gmail.com>
2024-06-14 17:57:03 +0530

    project complete..

--- currencyConverter/src/App.jsx
+++ currencyConverter/src/App.jsx
@@ -43,24 +43,35 @@ function App() {
                 label="From"
                 amount={amount}
                 currencyOptions={options}
+                onCurrencyChange={(currency) => setAmount(amount)}
+                onAmountChange={(amount) => setAmount(amount)}
+                selectCurrency={from}
               />
             </div>
             <div className="relative w-full h-0.5">
               <button
                 type="button"
                 className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                onClick={swap}
               >
                 swap
               </button>
             </div>
             <div className="w-full mt-1 mb-4">
-              <InputBox label="To" />
+              <InputBox
+                label="To"
+                amount={convertedAmount}
+                currencyOptions={options}
+                onCurrencyChange={(currency) => setTo(currency)}
+                selectCurrency={to}
+                amountDisable
+              />
             </div>
             <button
               type="submit"
               className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg"
             >
-              Convert
+              Convert {from.toUpperCase} to {to.toUpperCase}
             </button>
           </form>
         </div>
