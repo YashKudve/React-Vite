92692a320d4988fa2bad638fb788511418c591ac
Yash <kudveyash@gmail.com>
2024-06-14 16:55:03 +0530

    state management and functionality

--- currencyConverter/src/App.jsx
+++ currencyConverter/src/App.jsx
@@ -1,6 +1,23 @@
+import { useState } from "react";
 import InputBox from "./components/InputBox";
+import useCurrencyInfo from "../hooks/useCurrencyInfo";
 
 function App() {
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [convertedAmount, setConvertedAmount] = useState(0);
+
+  const currencyInfo = useCurrencyInfo(form);
+
+  const options = Object.keys(currencyInfo);
+
+  const swap = () => {
+    setFrom(to);
+    setTo(from);
+    setConvertedAmount(amount);
+    setAmount(convertedAmount);
+  };
   return (
     <>
       <h1 className="text-center font-extrabold text-[48px]">
